3be91eeedbf0dbf7d61407df6527678ab0fcd591
siwakorn-chantawat <siwakorn.chantawat@gmail.com>
2024-04-07 01:41:30 +0700

    remove unuse import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero";
 import { Highlight } from "@/components/Highlight";
-import { SectionHeading } from "@/components/SectionHeading";
 
 export default function Home() {
   return (
