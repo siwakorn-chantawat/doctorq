4df4505ec48000e85475486231701ca8dcd54f33
siwakorn-chantawat <siwakorn.chantawat@gmail.com>
2024-04-07 01:40:49 +0700

    add hilightdata, add blogs page

--- lib/data.ts
+++ lib/data.ts
@@ -1,16 +1,24 @@
 import React from "react"
 
 import image1 from "@/public/hero.png"
+import highlight1 from "@/public/oldman1.jpg";
+
 import { MdHome } from "react-icons/md"
 import { AiTwotoneFund } from "react-icons/ai"
 import { IoIosPricetag } from "react-icons/io"
 import { FaPhone } from "react-icons/fa6"
+import { BsBookFill } from "react-icons/bs";
 
 export const links = [
     {
         name: "Home",
         path: "/",
         icon: React.createElement(MdHome)
+    },
+    {
+        name: "Blogs",
+        path: "/blogs",
+        icon: React.createElement(BsBookFill)
     },
     {
         name: "Program",
@@ -55,8 +63,27 @@ export const carouselImagesData = [
 
 export const hilightCardsData = [
     {
-        title: "",
-        description: "",
-        imageURL: ""
-    }
+        title: "Test1",
+        description: "Doublo S Hifu เทคโนโลยียกกระชับผิว Hifu เทคโนโลยียกกระชับผิวที่มีประสิทธิภาพสูง พร้อมทั้งช่วยป้องกันความหย่อนคล้อยของผิวหนังบริเวณใบหน้า ลำคอ ใต้คาง",
+        imageURL: {highlight1},
+        id: 1 
+    },
+    {
+        title: "Test2",
+        description: "Doublo S Hifu เทคโนโลยียกกระชับผิว Hifu เทคโนโลยียกกระชับผิวที่มีประสิทธิภาพสูง พร้อมทั้งช่วยป้องกันความหย่อนคล้อยของผิวหนังบริเวณใบหน้า ลำคอ ใต้คาง",
+        imageURL: {highlight1},
+        id: 2 
+    },
+    {
+        title: "Test3 ",
+        description: "Doublo S Hifu เทคโนโลยียกกระชับผิว Hifu เทคโนโลยียกกระชับผิวที่มีประสิทธิภาพสูง พร้อมทั้งช่วยป้องกันความหย่อนคล้อยของผิวหนังบริเวณใบหน้า ลำคอ ใต้คาง",
+        imageURL: {highlight1},
+        id: 3
+    },
+    {
+        title: "Test4",
+        description: "Doublo S Hifu เทคโนโลยียกกระชับผิว Hifu เทคโนโลยียกกระชับผิวที่มีประสิทธิภาพสูง พร้อมทั้งช่วยป้องกันความหย่อนคล้อยของผิวหนังบริเวณใบหน้า ลำคอ ใต้คาง",
+        imageURL: {highlight1},
+        id: 4
+    },
 ] as const
