14018b0afb44779e14ab04ffd08971b149d15a89
siwakorn-chantawat <siwakorn.chantawat@gmail.com>
2024-04-11 00:56:29 +0700

    add maincarousel

---
+++ components/MainCarousel.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselDots,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { hilightCardsData } from "@/lib/data";
+import highlight1 from "@/public/oldman1.jpg";
+import { NewBlogCard } from "./NewBlogCard";
+import { ChevronsRightIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function MainCarousel() {
+  return (
+    <Carousel opts={{ align: "center" }} className="w-full">
+      <CarouselContent>
+        {hilightCardsData.map((card) => (
+          <CarouselItem key={card.id} className="md:basis-1/3">
+            <NewBlogCard {...card} imageURL={highlight1} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="hidden md:block" />
+      <CarouselNext className="hidden md:block" />
+      <div className="mt-4 flex flex-col gap-4 items-center justify-center">
+        <CarouselDots />
+
+        <Link
+          href="/blogs"
+          className="flex items-center text-primary gap-2 text-xl font-semibold"
+        >
+          <span> ดูทั้งหมด</span>
+          <ChevronsRightIcon />
+        </Link>
+      </div>
+    </Carousel>
+  );
+}
